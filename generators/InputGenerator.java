d4cbc67a4338bf6f6a9b7979257b8a36e5d53c29
Hezser <contact.sergiohernandez@gmail.com>
2018-10-20 02:29:47 +0100

    Minor changes to the input generator, creating constants for the number of formulas and their maximum length.

--- generators/InputGenerator.java
+++ generators/InputGenerator.java
@@ -4,17 +4,20 @@ import java.io.IOException;
 
 public class InputGenerator {
 
+    public static final int MAX_LENGTH = 50;  // Maximum length of a formula
+    public static final int N = 100;          // Number of formulas
+
     public static void main(String[] args) {
 
         String formulas = "";
         FormulaGenerator formulaGenerator = new FormulaGenerator();
 
         // Write an input file of 100 random formulas of a maximum length of 50 characters each
-        for (int i=0; i < 100; i++) {
+        for (int i=0; i < N; i++) {
             String formula = "";
             // Avoid propositions and negated propositions as full formulas
             while (formula.length() < 3) {
-                formula = formulaGenerator.generate(50);
+                formula = formulaGenerator.generate(MAX_LENGTH);
             }
             formulas += (formula + ";\n");
         }
