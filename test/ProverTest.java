0d599078ea31c0b722e9d36882466ccb08be8efe
Hezser <contact.sergiohernandez@gmail.com>
2018-10-25 20:50:25 +0100

    Small changes to documentation (Manual) and added a few tests for K systems.

--- test/ProverTest.java
+++ test/ProverTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 public class ProverTest {
 
     // Current Input File
@@ -26,7 +28,7 @@ public class ProverTest {
     // System K
 
     @Test
-    public void runRandomInputFileForSystemK() {
+    public void runRandomInputFile() {
         InputGenerator inputGenerator = new InputGenerator();
         inputGenerator.generateInputFile(10000, 50, "K");
 
@@ -35,12 +37,64 @@ public class ProverTest {
     }
 
     @Test
-    public void runRandomInputFileAndTranslateToMolleForSystemK() {
+    public void runRandomInputFileAndTranslateToMolle() {
         InputGenerator inputGenerator = new InputGenerator();
         inputGenerator.generateInputFileAndMolleFile(10000, 50, "K");
 
         Prover prover = new Prover(false);
         prover.proveInputFile();
+    }
+
+    @Test
+    public void molleAcceptsValidFormulas() {
+
+        Prover prover = new Prover(false);
+        ModalSystem system = new ModalSystem("K");
+        ArrayList<String> formulas = new ArrayList<>();
+
+        // Examples of valid formulas
+        formulas.add("((((q<->p)->p)|q)|(p<->q))"); formulas.add("(q<->q)");
+        formulas.add("(((q<->p)<->p)|((q|q)<->q))"); formulas.add("((p&q)->([]<>p|p))");
+        formulas.add("(~p|(p|(p&q)))"); formulas.add("[]((((p|~(q<->q))->p)|(q<->p))|q)");
+        formulas.add("([](q<->~~(q|(q&q)))|([]q&q))"); formulas.add("((q->q)->(q<->(q|q)))");
+        formulas.add("[][](q<->((q->q)->q))"); formulas.add("(((p&(p<->p))|(q->q))|((p|p)|q))");
+        formulas.add("((<>(p|p)&q)->(p->(q->q)))"); formulas.add("((p<->p)->(q|(p|(p->q))))");
+        formulas.add("(<>p->(p<->p))"); formulas.add("(~((q|q)->p)|(~~(q|[]<>~~p)|(q->q)))");
+        formulas.add("(~(([]q<->[](p->q))<->q)->(q<->q))"); formulas.add("[]((q->p)|q)");
+
+        for (String formula : formulas) {
+            try {
+                Assertions.assertTrue(prover.proveFormula(formula, system), ("The prover cannot validate " + formula));
+            } catch (UnrecognizableFormulaException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Test
+    public void molleRejectsInvalidFormulas() {
+
+        Prover prover = new Prover(false);
+        ModalSystem system = new ModalSystem("K");
+        ArrayList<String> formulas = new ArrayList<>();
+
+        // Examples of invalid formulas
+        formulas.add("((p<->p)&p)"); formulas.add("<>(<>p&(q->q))");
+        formulas.add("([](q<->(p<->q))->((p&<>(p|q))<->q))"); formulas.add("<>[]p");
+        formulas.add("~((<>[]~q|(p|(q&(q->q))))<->q)"); formulas.add("([]~[][](q&p)->p)");
+        formulas.add("(p&[](<>(~(p&q)->q)<->(p&q)))"); formulas.add("((<>q<->q)|[](q|([]<>p&q)))");
+        formulas.add("<>((p|q)<->((<>[](p<->(p|q))|q)->p))"); formulas.add("[]q");
+        formulas.add("<>q"); formulas.add("((~(<>(q<->q)<->q)|p)<->q)");
+        formulas.add("~~~~<>q"); formulas.add("(<>(<>q&q)&(q|~<>(p<->~[]q)))");
+        formulas.add("([]q&((((q->q)&p)<-><>(q|q))|q))"); formulas.add("<>((q<->((p->q)&q))<->p)");
+
+        for (String formula : formulas) {
+            try {
+                Assertions.assertFalse(prover.proveFormula(formula, system), ("The prover cannot invalidate " + formula));
+            } catch (UnrecognizableFormulaException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     // System T
